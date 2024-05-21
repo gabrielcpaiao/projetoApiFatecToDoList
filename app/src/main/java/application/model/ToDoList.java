2e79cf65fc1752136c2126efda5c5eb0b548dc17
Gabriel Paião <costapaiao@hotmail.com>
2024-05-21 19:37:24 +0000

    add

--- app/src/main/java/application/model/ToDoList.java
+++ app/src/main/java/application/model/ToDoList.java
@@ -18,18 +18,27 @@ public class ToDoList {
     
     private String date;
 
-    private 
-
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getTask() {
         return task;
     }
+
     public void setTask(String task) {
         this.task = task;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    
+    public void setDate(String date) {
+        this.date = date;
     }
 }
